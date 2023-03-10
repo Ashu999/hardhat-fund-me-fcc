d6218fb823589e720ca999cfab43da62954bfb27
Ashutosh Sharma <ashutosh.sharma068@gmail.com>
2023-03-10 12:55:13 +0530

    config: changed network from koven to sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,9 +15,8 @@ import { HardhatUserConfig } from "hardhat/config"
  */
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const KOVAN_RPC_URL =
-  process.env.KOVAN_RPC_URL ||
-  "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const RPC_URL =
+  process.env.RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
   "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
@@ -30,10 +29,11 @@ const config: HardhatUserConfig = {
       chainId: 31337,
       // gasPrice: 130000000000,
     },
-    kovan: {
-      url: KOVAN_RPC_URL,
+    sepolia: {
+      url: RPC_URL,
       accounts: [PRIVATE_KEY],
-      chainId: 42,
+      chainId: 11155111,
+      gasPrice: 35000000000,
     },
   },
   solidity: {
@@ -54,7 +54,7 @@ const config: HardhatUserConfig = {
     currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
-    // coinmarketcap: COINMARKETCAP_API_KEY,
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   namedAccounts: {
     deployer: {
